5cee38b365442b92239697388e56e0ba75545742
Riku Ruokonen <riku.ruokonen@sujuwa.fi>
2018-11-17 19:23:15 +0200

    Routing and List page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {
 } from "react-router-dom";
 import Home from './Containers/Home'
 import User from './Containers/User'
+import List from './Containers/List'
 import './App.css';
 
 class App extends Component {
@@ -17,10 +18,12 @@ class App extends Component {
                     <ul className="header">
                         <li><NavLink exact to="/">Home</NavLink> </li>
                         <li><NavLink to="/user">User</NavLink></li>
+                        <li><NavLink to="/list">List</NavLink></li>
                     </ul>
                   <div className="content">
                     <Route exact path="/" component={Home} />
-                    <Route path="/user" component={User} />
+                      <Route path="/user" component={User} />
+                    <Route path="/list" component={List} />
                   </div>
                 </div>
               </BrowserRouter>
---
+++ src/Containers/List.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class List extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h2>Vitusti listoja</h2>
+            </div>
+        );
+    }
+}
+
+export default List;
