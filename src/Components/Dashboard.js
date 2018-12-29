244c877e6d5bad8efa958e70a046323b85673d48
Riku Ruokonen <riku.ruokonen@sujuwa.fi>
2018-12-29 22:38:06 +0200

    siirretty kuvaaja vittuun homesta

---
+++ src/Components/Dashboard.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import 'react-vis/dist/style.css';
+import {
+    XYPlot,
+    VerticalBarSeries,
+    VerticalGridLines,
+    HorizontalGridLines,
+    XAxis,
+    YAxis
+} from 'react-vis';
+import '../App.css';
+import socketIOClient from "socket.io-client";
+
+import { API_URL } from '../config';
+
+let socket;
+
+class Dashboard extends Component {
+
+    constructor(props) {
+        super()
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <div className="chart-container">
+                    <XYPlot
+                        xType="ordinal"
+                        stackBy="y"
+                        width={300}
+                        height={300}
+                    >
+                        <VerticalGridLines />
+                        <HorizontalGridLines />
+                        <XAxis />
+                        <YAxis />
+                        <VerticalBarSeries
+                            color="#12939A"
+                            data={this.props.data}
+                        />
+                    </XYPlot>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Dashboard;
--- src/Containers/Home.js
+++ src/Containers/Home.js
@@ -12,6 +12,7 @@ import '../App.css';
 import socketIOClient from "socket.io-client";
 
 import { API_URL } from '../config';
+import Dashboard from "../Components/Dashboard";
 
 let socket;
 
@@ -35,24 +36,8 @@ class Home extends Component {
 
     render() {
         return (
-            <div className="App">
-                <div className="chart-container">
-                    <XYPlot
-                        xType="ordinal"
-                        stackBy="y"
-                        width={300}
-                        height={300}
-                    >
-                        <VerticalGridLines />
-                        <HorizontalGridLines />
-                        <XAxis />
-                        <YAxis />
-                        <VerticalBarSeries
-                            color="#12939A"
-                            data={this.state.data}
-                        />
-                    </XYPlot>
-                </div>
+            <div>
+                <Dashboard data={this.state.data}/>
             </div>
         );
     }
