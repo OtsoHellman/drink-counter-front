0f251a5f526b25da18bc73e1e72bd947c703fe26
otsohellman <otso.hellman@gmail.com>
2019-01-25 18:55:12 +0200

    unspaghettify snackbar impementation

--- src/Containers/User.js
+++ src/Containers/User.js
@@ -5,16 +5,7 @@ import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
 import { withStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
-import Snackbar from '@material-ui/core/Snackbar';
-import ErrorIcon from '@material-ui/icons/Error';
-import CloseIcon from '@material-ui/icons/Close';
-import green from '@material-ui/core/colors/green';
-import amber from '@material-ui/core/colors/amber';
-import IconButton from '@material-ui/core/IconButton';
-import classNames from 'classnames';
-import PropTypes from 'prop-types';
-import CheckCircleIcon from '@material-ui/icons/CheckCircle';
-import SnackbarContent from '@material-ui/core/SnackbarContent';
+import StatusSnackBar from '../Components/StatusSnackBar';
 
 import { getUserData, addDrinkByUsername, getDrinkTypes, addDrinkType } from '../utils/helpers';
 
@@ -40,74 +31,6 @@ const styles = theme => ({
         margin: theme.spacing.unit,
     }
 });
-
-const styles1 = theme => ({
-    success: {
-        backgroundColor: green[600],
-    },
-    error: {
-        backgroundColor: theme.palette.error.dark,
-    },
-    warning: {
-        backgroundColor: amber[700],
-    },
-    icon: {
-        fontSize: 20,
-    },
-    iconVariant: {
-        opacity: 0.9,
-        marginRight: theme.spacing.unit,
-    },
-    message: {
-        display: 'flex',
-        alignItems: 'center',
-    },
-});
-
-const variantIcon = {
-    success: CheckCircleIcon,
-    error: ErrorIcon,
-};
-
-function MySnackbarContent(props) {
-    const { classes, className, message, onClose, variant, ...other } = props;
-    const Icon = variantIcon[variant];
-
-    return (
-        <SnackbarContent
-            className={classNames(classes[variant], className)}
-            aria-describedby="client-snackbar"
-            message={
-                <span id="client-snackbar" className={classes.message}>
-                    <Icon className={classNames(classes.icon, classes.iconVariant)} />
-                    {message}
-                </span>
-            }
-            action={[
-                <IconButton
-                    key="close"
-                    aria-label="Close"
-                    color="inherit"
-                    className={classes.close}
-                    onClick={onClose}
-                >
-                    <CloseIcon className={classes.icon} />
-                </IconButton>,
-            ]}
-            {...other}
-        />
-    );
-}
-
-MySnackbarContent.propTypes = {
-    classes: PropTypes.object.isRequired,
-    className: PropTypes.string,
-    message: PropTypes.node,
-    onClose: PropTypes.func,
-    variant: PropTypes.oneOf(['success', 'warning', 'error', 'info']).isRequired,
-};
-
-const MySnackbarContentWrapper = withStyles(styles1)(MySnackbarContent);
 
 class User extends Component {
 
@@ -146,8 +69,7 @@ class User extends Component {
                     username: res.data.username,
                     gender: res.data.gender,
                     konni: res.data.konni,
-                    drinkMap: res.data.drinkMap,
-                    keysSorted: res.data.keysSorted
+                    drinkMap: res.data.drinkMap
                 })
             })
     }
@@ -158,8 +80,8 @@ class User extends Component {
             errorSnackBarOpen: false
         })
         addDrinkByUsername(this.props.location.pathname.substring(6), drinkTypeId)
-            .then((asd) => {
-                if (asd.status === 200) {
+            .then((res) => {
+                if (res.status === 200) {
                     this.setState({
                         successfulSnackBarOpen: true
                     })
@@ -188,26 +110,29 @@ class User extends Component {
 
     addDrinkType = () => {
         addDrinkType(this.state.drinkName, this.state.drinkSize)
+            .then((res) => {
+                if (res.status === 200) {
+                    this.setState({
+                        successfulSnackBarOpen: true
+                    })
+                } else {
+                    this.setState({
+                        errorSnackBarOpen: true
+                    })
+                }
+            })
             .then(() => {
                 this.getUserData()
                 this.getDrinkData()
             })
     }
 
-    handleSuccessfulClose = (event, reason) => {
+    handleClose = (event, reason) => {
         if (reason === 'clickaway') {
             return;
         }
         this.setState({
-            successfulSnackBarOpen: false
-        })
-    }
-
-    handleErrorClose = (event, reason) => {
-        if (reason === 'clickaway') {
-            return;
-        }
-        this.setState({
+            successfulSnackBarOpen: false,
             errorSnackBarOpen: false
         })
     }
@@ -264,39 +189,13 @@ class User extends Component {
                         Submit
                             </Button>
                 </form>
-                <Snackbar
-                    anchorOrigin={{
-                        vertical: 'bottom',
-                        horizontal: 'left',
-                    }}
-                    open={this.state.successfulSnackBarOpen}
-                    autoHideDuration={3000}
-                    onClose={this.handleSuccessfulClose}
-                >
-                    <MySnackbarContentWrapper
-                        onClose={this.handleSuccessfulClose}
-                        variant="success"
-                        className={classes.margin}
-                        message="Drink added successfully!"
-                    />
-                </Snackbar>
-
-                <Snackbar
-                    anchorOrigin={{
-                        vertical: 'bottom',
-                        horizontal: 'left',
-                    }}
-                    open={this.state.errorSnackBarOpen}
-                    autoHideDuration={3000}
-                    onClose={this.handleErrorClose}
-                >
-                    <MySnackbarContentWrapper
-                        onClose={this.handleErrorClose}
-                        variant="error"
-                        className={classes.margin}
-                        message="Invalid drink size!"
-                    />
-                </Snackbar>
+                <StatusSnackBar
+                    successMessage="Drink added successfully!"
+                    errorMessage="Invalid drink size!"
+                    successfulSnackBarOpen={this.state.successfulSnackBarOpen}
+                    errorSnackBarOpen={this.state.errorSnackBarOpen}
+                    handleClose={this.handleClose}
+                />
             </div>
         );
     }
